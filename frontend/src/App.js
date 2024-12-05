659663e9d2bd87d4f56d58c0161f01519bcec8eb
RyanChehab <ryanchehab20@gmail.com>
2024-12-05 02:45:57 +0200

    js <=> imported Question Provider in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,21 @@ import Login from "./components/login";
 import Signup from "./components/signup";
 import Sheet from "./components/quiz_sheet";
 import { MyProvider } from "./context/Auth";
+import {QuestionProvider} from "./context/Quiz";
 import  {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 function App() {
   return (
-    <MyProvider>
-      <Router>
-        <Routes>
-            <Route path="/" element={<Login/>} />
-            <Route path="signup" element={<Signup/>} />
-            <Route path="quiz" element={<Sheet/>} />
-        </Routes>
-      </Router>
-    </MyProvider>
+    <QuestionProvider>
+      <MyProvider>
+        <Router>
+          <Routes>
+              <Route path="/" element={<Login/>} />
+              <Route path="signup" element={<Signup/>} />
+              <Route path="quiz" element={<Sheet/>} />
+          </Routes>
+        </Router>
+      </MyProvider>
+    </QuestionProvider>
   );
 }
 
