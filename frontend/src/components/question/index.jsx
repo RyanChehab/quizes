3b395b9d9f39d5f668e97e3194ca4011f09fce40
RyanChehab <ryanchehab20@gmail.com>
2024-12-06 03:05:01 +0200

    jsx <=> fixed the ui of the questions

--- frontend/src/components/question/index.jsx
+++ frontend/src/components/question/index.jsx
@@ -1,2 +1,23 @@
-import React from 'react';
+import React,{ useContext }  from 'react';
+import { QuestionContext } from '../../context/Quiz';
+import "./question.css";
 
+const Question = ({ question, index }) => {
+    const { responses, updateResponse } = useContext(QuestionContext);
+
+    return (
+        <div className='question'>
+            <h4>{question}</h4>
+            <label>Response: </label>
+            <input
+                type="text"
+                value={responses[index]} // Bind to the current response
+                onChange={(e) => updateResponse(index, e.target.value)} // Update the response
+            />
+            <br /><br />
+            <hr />
+        </div>
+    );
+};
+
+export default Question;
