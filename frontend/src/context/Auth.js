5cb575f39821d27580436c01c2a74d23f7435070
RyanChehab <ryanchehab20@gmail.com>
2024-12-07 21:03:26 +0200

    js <=> updated the handle submit function

--- frontend/src/context/Auth.js
+++ frontend/src/context/Auth.js
@@ -1,4 +1,5 @@
 import { useState, createContext } from "react";
+import useFetch from "../hooks/fetch";
 
 const AuthContext = createContext();
 
@@ -11,13 +12,20 @@ const MyProvider = ({ children }) => {
     const [name,setName] = useState("");
     const [registerEmail,setRegisterEmail] = useState("");
     const [registerPassword,setregisterPassword] = useState("");
+
+    const handleSignup = (e,type)=>{
+        e.preventDefault()
+        console.log("working")
+        console.log(type)
+    }
     
     return (
         <AuthContext.Provider value={{ email, setEmail,
         password,setPassword,
         name,setName,
         registerEmail,setRegisterEmail,
-        registerPassword,setregisterPassword
+        registerPassword,setregisterPassword,
+        handleSignup
         }}>
             {children}
         </AuthContext.Provider>
