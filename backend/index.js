2aa4070a75486a8b5727dc3c2c6acacf84716262
RyanChehab <ryanchehab20@gmail.com>
2024-12-07 03:01:07 +0200

    js <=> ran the server

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,10 @@
 import express from 'express';
+
+
+const app = express();
+
+
+const PORT = process.env.PORT
+app.listen(PORT, ()=>{
+    console.log("Server running on http://localhost:${PORT}")
+})
