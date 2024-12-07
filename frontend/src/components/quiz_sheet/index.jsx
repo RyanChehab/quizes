55add4ca3c2f15d9c049ea9fbe9038631b91c166
RyanChehab <ryanchehab20@gmail.com>
2024-12-07 03:11:14 +0200

    jsx <=> fixed the quiz sheet logic

--- frontend/src/components/quiz_sheet/index.jsx
+++ frontend/src/components/quiz_sheet/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useContext} from "react";
 import '../../utilities.css';
 import './sheet.css';
 import Question from "../question";
@@ -6,12 +6,16 @@ import { QuestionContext } from '../../context/Quiz';
 
 const Sheet = ()=>{
 
+    const { questions } = useContext(QuestionContext);
+
     return(
         <>
         <div>
             <h1 className="Quiz">Quiz</h1>
             <div className="sheet">
-                <Question></Question>
+            {questions.map((question, index) => (
+                <Question key={index} question={question} index={index} />
+            ))}
             </div>
 
         </div>
