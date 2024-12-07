3a0996d00a8d43af3cf662065a18edb5d4ae3771
RyanChehab <ryanchehab20@gmail.com>
2024-12-07 14:18:39 +0200

    js <=> created progress schema

---
+++ backend/shcemas/progress.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+
+const progressSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', 
+    required: true,
+  },
+  quizId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Quiz',
+    required: true,
+  },
+  score: {
+    type: Number,
+    default: 0,
+  },
+  completed: {
+    type: Boolean,
+    default: false, 
+  },
+  answers: [
+    {
+      questionId: {
+        type: mongoose.Schema.Types.ObjectId, 
+        required: true,
+      },
+      userAnswer: {
+        type: String,
+        required: true,
+      },
+      correct: {
+        type: Boolean, 
+        default: false,
+      },
+    },
+  ],
+});
+
+const UserQuizProgress = mongoose.model('UserQuizProgress', progressSchema);
+export default UserQuizProgress;
