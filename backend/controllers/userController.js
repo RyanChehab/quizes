690902a6022b2b01ef7540e13c48dd8ede37ed4a
RyanChehab <ryanchehab20@gmail.com>
2024-12-07 16:43:11 +0200

    js <=> created signup logic

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,11 +4,23 @@ export const registerUser = async (req,res) =>{
     try{
         const {name, email,password} = req.body;
 
-        const userExists = await user.findone({email});
+        const userExists = await User.findone({email});
         if(userExists){
             return res.status(400).json({ message: 'User already exists' });
         }
-    }catch(error){
 
+        const user = await User.create({ name, email, password });
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: '1h',
+          });
+      
+          res.status(201).json({
+            message: 'User registered successfully',
+            token,
+          });
+
+    }catch(error){
+        res.status(500).json({ message: 'Error registering user', error });
     }
 }
