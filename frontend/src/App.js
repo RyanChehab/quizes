48e64a8423d3f0901f6d6bdfa9d1da0f61a1eaeb
RyanChehab <ryanchehab20@gmail.com>
2024-12-07 22:51:41 +0200

    js <=> fixed the routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,19 @@ import Signup from "./components/signup";
 import Sheet from "./components/quiz_sheet";
 import { MyProvider } from "./context/Auth";
 import {QuestionProvider} from "./context/Quiz";
-import  {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import  {BrowserRouter, Routes, Route} from "react-router-dom";
 function App() {
   return (
     <QuestionProvider>
-      <MyProvider>
-        <Router>
-          <Routes>
-              <Route path="/" element={<Login/>} />
-              <Route path="signup" element={<Signup/>} />
-              <Route path="quiz" element={<Sheet/>} />
-          </Routes>
-        </Router>
-      </MyProvider>
+        <BrowserRouter>
+        <MyProvider>
+            <Routes>
+                <Route path="/" element={<Login/>} />
+                <Route path="signup" element={<Signup/>} />
+                <Route path="quiz" element={<Sheet/>} />
+            </Routes>
+          </MyProvider>
+        </BrowserRouter>
     </QuestionProvider>
   );
 }
