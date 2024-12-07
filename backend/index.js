c327d00f13e68ae815c4b820d975478672a02c5e
RyanChehab <ryanchehab20@gmail.com>
2024-12-07 03:10:33 +0200

    js <=> connected to database

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
+dotenv.config()
 
 const app = express();
 
+mongoose.connect(process.env.DB_URL)
+.then(()=>console.log("connected"))
+.catch((err)=> console.error('failed to connect'))
 
 const PORT = process.env.PORT
+
 app.listen(PORT, ()=>{
-    console.log("Server running on http://localhost:${PORT}")
+    console.log(`Server running on http://localhost:${PORT}`)
 })
