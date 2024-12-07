365ca4d3502aed93f21da0355d6abc98d1a44072
RyanChehab <ryanchehab20@gmail.com>
2024-12-07 17:02:49 +0200

    js <=> imported routes in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-
+import userRoutes from './routes.js '
 dotenv.config()
 
 const app = express();
 
+// connection to db
 mongoose.connect(process.env.DB_URL)
 .then(()=>console.log("connected"))
 .catch((err)=> console.error('failed to connect'))
 
-const PORT = process.env.PORT
+app.use('api/users', userRoutes)
 
+const PORT = process.env.PORT
 app.listen(PORT, ()=>{
     console.log(`Server running on http://localhost:${PORT}`)
 })
